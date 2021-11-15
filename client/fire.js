8fcd9a267871f4e593e6508705432529177307f6
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-15 16:55:41 +1300

    added firebase config / fixed html title

---
+++ client/fire.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsm3MOhV0FcK4_shyRJkJMHBHxwXR32yc",
+  authDomain: "bug-tracker-66294.firebaseapp.com",
+  projectId: "bug-tracker-66294",
+  storageBucket: "bug-tracker-66294.appspot.com",
+  messagingSenderId: "36423460663",
+  appId: "1:36423460663:web:c11ec41a761a40946f9cb2"
+};
+
+
+const fire = initializeApp(firebaseConfig);
+
+export default fire;
