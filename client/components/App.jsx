4c0637f4885770e6eafb621dc26caedd9afc3d08
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-15 20:38:39 +1300

    removed all auth funcs because they were built for outdated firbase version lmao

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,63 +10,7 @@ function App () {
   const [passwordError, setPasswordError] = useState('')
   const [hasAccount, setHasAccount] = useState(false)
 
-  // Handles user sign in / updates state with error messages if any are made
-  const handleLogIn = () => {
-    fire
-      auth()
-      signInWithEmailAndPassword(email, password)
-      .catch((err) => {
-        switch (err.code){
-          case "auth/invalid-email":
-          case "auth/user-disabled":
-          case "auth/user-not-found":
-            setEmailError(err.message);
-            break;
-          case "auth/wrong-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-
-  // Handles user sign up / updates state with error messages if any are made
-  const handleSignUp = () => {
-    fire
-      auth()
-      createUserWithEmailAndPassword(email, password)
-      .catch((err) => {
-        switch (err.code){
-          case "auth/email-already-in-use":
-          case "auth/Invalid-email ":
-            setEmailError(err.message);
-            break;
-          case "auth/weak -password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-
-  // Signs users out of logged in account
-  const handleSignOut = () => {
-    fire.auth().signOut();
-  }
-
-  // Updates user state if valid user found
-  const authListener = () => {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user)
-      } else {
-        setUser('')
-      }
-    })
-  }
-
-  // Checks for user on load
-  useEffect(() => {
-    authListener()
-  }, [])
+  
 
   return (
       <div className='app'>
