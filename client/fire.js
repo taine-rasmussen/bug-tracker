0988f7ccddd7102a5721a4b6fc80390729260579
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-15 20:54:36 +1300

    updated fire.js with firebase v9

--- client/fire.js
+++ client/fire.js
@@ -1,18 +1,17 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsm3MOhV0FcK4_shyRJkJMHBHxwXR32yc",
-  authDomain: "bug-tracker-66294.firebaseapp.com",
-  projectId: "bug-tracker-66294",
-  storageBucket: "bug-tracker-66294.appspot.com",
-  messagingSenderId: "36423460663",
-  appId: "1:36423460663:web:c11ec41a761a40946f9cb2"
+  apiKey: "AIzaSyAXFme_oTPlrFmWIm707ac6Z2qawoTvf2k",
+  authDomain: "authentication-tutorial-ec556.firebaseapp.com",
+  projectId: "authentication-tutorial-ec556",
+  storageBucket: "authentication-tutorial-ec556.appspot.com",
+  messagingSenderId: "945663559539",
+  appId: "1:945663559539:web:51b9786fd75d399bf86706",
+  measurementId: "G-22FP5PHT1F",
 };
-
 
 const fire = initializeApp(firebaseConfig);
 
-export default fire;
+export const auth = getAuth(fire);
