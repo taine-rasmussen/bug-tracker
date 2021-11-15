2ee723f7b7462252e273506ee3cf4c8582d9d065
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-15 21:09:28 +1300

    updated details of firebase-config

---
+++ client/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsm3MOhV0FcK4_shyRJkJMHBHxwXR32yc",
+  authDomain: "bug-tracker-66294.firebaseapp.com",
+  projectId: "bug-tracker-66294",
+  storageBucket: "bug-tracker-66294.appspot.com",
+  messagingSenderId: "36423460663",
+  appId: "1:36423460663:web:c11ec41a761a40946f9cb2"
+};
+
+const fire = initializeApp(firebaseConfig);
+
+export const auth = getAuth(fire);
