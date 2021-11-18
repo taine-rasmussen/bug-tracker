aa17cf30c83b7684df51e094aa0fb23864eb1dd7
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-18 14:26:54 +1300

    set up connection to firestore - data being received

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 
 // Components
 import Nav from './Nav'
 
 
 const Dashboard = () => {
+
+   const [projectData, setProjectData] = useState([])
+   const projectCollectionRef = collection(db, 'projects')
+
+
+   useEffect(() => {
+      const getProjects = async () => {
+         const data = await getDocs(projectCollectionRef)
+         console.log(data)
+      }
+      getProjects()
+   }, [])
 
    return (
       <div className='dashboard-container'>
