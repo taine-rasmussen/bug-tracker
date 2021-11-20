dc035f7a5ebfd2e2c3787d1256cf8f5c546c2a49
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-20 13:31:47 +1300

    added dynamic stlying based off priortiy status

--- client/components/ProjectDisplay.jsx
+++ client/components/ProjectDisplay.jsx
@@ -4,25 +4,49 @@ import '../styles/ProjectDisplay.css'
 const ProjectDisplay = ({projectData}) => {
 
 
+  const highPriorityStyle =  {
+      'backgroundColor': '#ff5760',
+      'borderBottom': '1px solid #ff5760',
+      'borderRadius': '5px',
+      'color': '#222b42',
+      'padding': '0px 15px'
+   }
+      
+   const lowPriorityStyle = {
+         'backgroundColor': '#ffffff',
+         'borderBottom': '1px solid #ffffff',
+         'borderRadius': '5px',
+         'color': '#222b42',
+         'padding': '0px 15px'
+
+   }
+   
+
+
+
+
 
    return (
       <div className="project-display-container">
          {projectData.map((proj, index) => {
             return(
                <div className="project-single-container" key={index}>
+                  <div className="project-header">
+                     <h2>Project Name: {proj.Name}</h2>
 
-                  <div className="project-header-container">
-                     <div className='project-name'>
-                        <h2>Project Name: {proj.Name}</h2>
+
+                     <div 
+                        className="project-priority" 
+                        style={proj.Priority === 'High' ? 
+                        highPriorityStyle : lowPriorityStyle}
+                     >
+                        <h2>{proj.Priority}</h2>
                      </div>
-                     <div className='project-priority'>
-                        <h2>Priority: {proj.Priority}</h2>
-                     </div>
-                  
-                  
-                  
                   </div>
-                  {/* <h2>Issue: {proj.Issue.title}</h2> */}
+
+
+
+
                </div>
             )
          })}
