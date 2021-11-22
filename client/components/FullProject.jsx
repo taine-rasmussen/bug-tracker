daea98855370a0f4b2d5be09ac927a2994f9b642
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-23 11:34:34 +1300

    cleaned up fullproject

--- client/components/FullProject.jsx
+++ client/components/FullProject.jsx
@@ -29,23 +29,24 @@ const FullProject = ({proj}) => {
 
    return (
      <div className='project-issues-container'>
-                     {proj.Issues.map((issue, index) => {
-                        return(
-                              <div className='project-body' index={index}>
-                                 <div className='issue-description'>
-                                    <h3 className='description-title'><span>Description:</span></h3> <p>{issue.Description}</p>
-                                    <h3><span>Comments:</span> {issue.Comments.length}</h3>
-                                 </div>
-                                  <div 
-                                    className="project-priority" 
-                                    style={issue.Priority === 'High' ? 
-                                    highPriorityStyle : lowPriorityStyle}
-                                 >
-                                    <h4>{issue.Priority}</h4>
-                                 </div>
-                              </div>
-                        )
-                     })}
+         {proj.Issues.map((issue, index) => {
+            return(
+               <div className='project-body' index={index}>
+                  <div className='issue-description'>
+                     <h3 className='description-title'><span>Description:</span></h3>
+                      <p>{issue.Description}</p>
+                     <h3><span>Comments:</span> {issue.Comments.length}</h3>
+                  </div>
+                  <div 
+                     className="project-priority" 
+                     style={issue.Priority === 'High' ? 
+                     highPriorityStyle : lowPriorityStyle}
+                  >
+                     <h4>{issue.Priority}</h4>
+                  </div>
+               </div>
+            )
+         })}
       </div>
    )
 }
