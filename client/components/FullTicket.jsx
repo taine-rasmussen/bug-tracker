1041a7d9965c279e3fcdd04d2782fe715d4f1c92
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-23 21:47:48 +1300

    rendered data and styled full ticket info

--- client/components/FullTicket.jsx
+++ client/components/FullTicket.jsx
@@ -4,7 +4,29 @@ import '../styles/FullTicket.css'
 const FullTicket = ({fullIssueData, setViewPreview}) => {
    return (
       <div className="ticket-container">
-         <button onClick={() => setViewPreview(false)}>Close</button>
+         <div className="ticket-header">
+            <h2>{fullIssueData.Issue}:</h2>
+            <button onClick={() => setViewPreview(false)}>Close</button>
+         </div>
+         <div className="ticket-body">
+            <div className='ticket-description'>
+               <h4>Description:</h4>
+               <p>{fullIssueData.Description}</p>
+            </div>
+            <div>
+               {fullIssueData.Priority}
+            </div>
+         </div>
+         <div className='ticket-footer'>
+            <h4>Comments:</h4>
+            {fullIssueData.Comments.map((coms) => {
+               return(
+                  <p key={fullIssueData.Id}>{coms}</p>
+               )
+            })}
+
+         </div>
+         
       </div>
    )
 }
